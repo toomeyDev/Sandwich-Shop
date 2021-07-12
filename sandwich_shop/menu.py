efd1bbe0c1f792c3f042302fc4034cafc1185e19
Matthew <mtoomeydev@gmail.com>
2021-07-12 13:02:34 -0400

    beginning structure of restaurant class

--- sandwich_shop/menu.py
+++ sandwich_shop/menu.py
@@ -3,13 +3,23 @@ import sandwich
 
 class Menu :
     """Represents a larger menu (IE restaurants with no specific breakfast/lunch menu)."""
-    def __init__(self, name: 'str') :
+    def __init__(self, name: str='default') :
         """Initialize menu contents with provided values."""
         # create named dictionary to store menu items
         self.contents = {
             name: {}
         }
+    
+    def add_item(self, item: sandwich.Sandwich, name: str) :
+        """Add a new item to the contents of the menu."""
+        self.contents[name] = item
         
+    def get_contents(self):
+        """Retrieve the contents of this menu object."""
+        menu_string = ''
+        for item in self.contents:
+            menu_string += item + '\n'
+        return menu_string
 
 class SubMenu(Menu) :
     """Represents a smaller, sub-menu (IE breakfast menu, lunch menu)."""
--- sandwich_shop/sandwich_shop.py
+++ sandwich_shop/sandwich_shop.py
@@ -1,2 +1,6 @@
 """Manages game-flow and sequencing of events."""
-import restaurant
+from restaurant import Restaurant
+
+test_location = Restaurant('Testing Grounds', 500)
+test_location.add_menu_item("p")
+test_location.show_info()

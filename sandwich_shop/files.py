afd65693a20ac9a064992cd802277c47c73053ac
Matthew <mtoomeydev@gmail.com>
2021-07-31 18:24:31 -0400

    added files.py to handle saving, loading, scoreboard functionality

---
+++ sandwich_shop/files.py
@@ -0,0 +1,11 @@
+"""
+Handle player saves (writing and loading), scoreboard, read/write of text or .json files
+"""
+
+# awaiting implementation
+
+# def save_game():
+#     # pending save logic, should save the player's current information to a .json file
+
+# def load_game():
+#     # pending load logic, should load a player's .json save file and return simulation to the state at which the save was made
